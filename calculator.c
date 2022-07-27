5943f4c1f1249608481b71e3cb3cc83d3076375e
Satyam-kaushik <satyamkaushik709@gmail.com>
2022-07-27 22:11:44 +0530

    remainder

--- calculator.c
+++ calculator.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
- 
+
+void Addition(){
+    float x,y;
+    printf("Enter the two numbers x and y for addition respectively : ");
+    scanf("%f%f",&x,&y);
+    printf("The addition is %f",x+y);
+}
+void Subtraction(){
+    float x,y;
+    printf("Enter the two numbers x and y for subtraction respectively : ");
+    scanf("%f%f",&x,&y);
+    printf("The subtraction %d-%d is %f",x,y,x-y);
+}
+void Multiplication(){
+    float x,y;
+    printf("Enter the two numbers x and y for multiplication respectively : ");
+    scanf("%f%f",&x,&y);
+    printf("The multiplication is %f",x*y);
+}
+void Division(){
+    float x,y;
+    printf("Enter the two numbers x and y for division respectively : ");
+    scanf("%f%f",&x,&y);
+    printf("The division %f/%f is %f",x,y,x/y);
+}
+void Remainder(){
+    int x,y;
+    printf("Enter the two integers x and y for remainder respectively : ");
+    scanf("%d%d",&x,&y);
+    printf("The remainder is %d",x%y);
+}
+
 int main()
 {
-   float a,b;
-   printf("Enter the numbers  : ");
-   scanf("%f%f",&a,&b);
+    int option;
+    printf("Enter the following among 1,2,3,4,5 for the following option \n");
+    printf("1.Addition \n2.Subtraction\n3.Mutiplication\n4.Division\n5.Remainder\n");
+    scanf("%d", &option);
 
-   printf("The sum of two numbers is %f\n",a+b);
-   printf("The differnce of two numbers is %f\n",a-b);
-   printf("The multiplication of two numbers is %f\n",a*b);
-   printf("The division of the numbers is %f\n",a/b);
+    switch (option)
+    {
+    case 1:
+        Addition();
+        break;
+    case 2:
+        Subtraction();
+        break;
+    case 3:
+        Multiplication();
+        break;
+    case 4:
+        Division();
+        break;
+    case 5:
+        Remainder();
+        break;
+    }
 
         return 0;
     }
-
-    
