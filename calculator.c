ed93888d37ce0485d65be0d87c4cc4dc4ea5fa87
Satyam-kaushik <satyamkaushik709@gmail.com>
2022-07-27 21:55:39 +0530

    addition

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main()
+{
+   float a,b;
+   printf("Enter the numbers for addition : ");
+   scanf("%f%f",&a,&b);
+
+   printf("The sum of two numbers is %f",a+b);
+        return 0;
+    }
