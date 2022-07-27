6dc46c9c8ae93d69b44a73cd4951aa9f711c3d45
Satyam-kaushik <satyamkaushik709@gmail.com>
2022-07-27 22:02:37 +0530

    subtraction

--- calculator.c
+++ calculator.c
@@ -3,9 +3,13 @@
 int main()
 {
    float a,b;
-   printf("Enter the numbers for addition : ");
+   printf("Enter the numbers  : ");
    scanf("%f%f",&a,&b);
 
-   printf("The sum of two numbers is %f",a+b);
+   printf("The sum of two numbers is %f\n",a+b);
+   printf("The differnce of two numbers is %f\n",a-b);
+
         return 0;
     }
+
+    
